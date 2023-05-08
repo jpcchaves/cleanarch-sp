6147f5e58f77b9646ae945be69aed076e9bf7eee
João Paulo <jpcchaves@outlook.com>
2023-05-08 12:50:05 -0300

    initial commit

---
+++ src/test/java/com/jpcchaves/booksys/BooksysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpcchaves.booksys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
